243c0e5e9b25274b6b9a252030ad4926a4c3cf51
ardyutama <ardy.putra@pinhome.id>
2023-08-14 22:56:22 +0700

    feat: add transition between page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,16 @@
 <script>
-  import "../app.css";
-  import Footer from "./Footer.svelte";
-  import Navbar from "./Navbar.svelte";
+	import '../app.css';
+	import { fade } from 'svelte/transition';
+	import Footer from './Footer.svelte';
+	import Navbar from './Navbar.svelte';
+
+	export let data;
 </script>
 
 <Navbar />
-  <slot />
+{#key data.pathname}
+	<div in:fade={{ duration: 300, delay: 400 }} out:fade={{ duration: 300 }}>
+		<slot />
+	</div>
+{/key}
 <Footer />
